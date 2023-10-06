6ec54f71d7138287b4ccee5dfb3eeeb9c1d004a2
Leonardo Machado Morozini - 1292310673 <1292310673@ulife.com.br>
2023-10-06 20:06:31 -0300

    Classe client_socialnetwork e socialnetwork

---
+++ client_socialnetwork.py
@@ -0,0 +1,4 @@
+
+from client import Client
+
+from socialnetwork import Socialnetwork
---
+++ socialnetwork.py
@@ -0,0 +1,5 @@
+class Socialnetwork:
+
+    def __init__(self, name, description):
+        self._name = name
+        self._description = description
