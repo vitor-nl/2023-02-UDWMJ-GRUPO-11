8a5e39daf56157865ccf225d73acaa9d040ded5c
Lucas Rodrigues Lança - 1292216966 <1292216966@ulife.com.br>
2023-10-06 20:48:37 -0300

    comitando classe Order

---
+++ Order.py
@@ -0,0 +1,7 @@
+from Client import Client
+
+class Order:
+    def __init__(self, total_price, status, client):
+        self.total_price = total_price
+        self.status = status
+        self.client = Client
