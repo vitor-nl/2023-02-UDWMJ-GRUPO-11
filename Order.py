a995b74252b8a2f334854417b887673ff15cf922
Lucas Rodrigues Lança - 1292216966 <1292216966@ulife.com.br>
2023-10-06 21:12:49 -0300

    dependecias certas, tudo certo

--- Order.py
+++ Order.py
@@ -1,7 +1,7 @@
 from Client import Client
 
 class Order:
-    def __init__(self, total_price, status, client):
+    def __init__(self, total_price, status, client : Client):
         self.total_price = total_price
         self.status = status
-        self.client = Client
+        self.client = client
--- Product.py
+++ Product.py
@@ -1,9 +1,10 @@
+from nis import cat
 from Category import Category
 
 class Product:
-    def __init__(self, name, description, date_fabrication, is_active, category):
+    def __init__(self, name, description, date_fabrication, is_active, category : Category):
         self.name = name
         self.description = description
         self.date_fabrication = date_fabrication
         self.is_active = is_active
-        self.category = Category
+        self.category = category
