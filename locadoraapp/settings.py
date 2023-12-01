4147107d2f0b82e2ddea8656a13448bfe1f815d7
brunabertoldop <brunabertoldop@gmail.com>
2023-12-01 19:09:58 -0300

    parte 7 - orders

--- locadoraapp/settings.py
+++ locadoraapp/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'products.apps.ProductsConfig',
     'socialnetworks.apps.SocialnetworksConfig',
     'clients.apps.ClientsConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
--- locadoraapp/urls.py
+++ locadoraapp/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('redessociais/', include('socialnetworks.urls', namespace='socialnetworks')),
     path('clientes/', include('clients.urls', namespace='clients')),
     path('clientes_redessociais/', include('clients.urls', namespace='clients_socialnetworks')),
+    path('pedidos/', include('orders.urls', namespace='orders')),
+    path('pedidos_itens/', include('orders.urls', namespace='orders_items')),
 ]
