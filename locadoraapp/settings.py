f4ed58c07e72061ef93fdf7fd4a517be4fb37123
brunabertoldop <brunabertoldop@gmail.com>
2023-12-01 19:00:28 -0300

    parte 5 - socialnetworks

--- locadoraapp/settings.py
+++ locadoraapp/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'categories.apps.CategoriesConfig',
     'products.apps.ProductsConfig',
+    'socialnetworks.apps.SocialnetworksConfig',
 ]
 
 MIDDLEWARE = [
--- locadoraapp/urls.py
+++ locadoraapp/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('categorias/', include('categories.urls', namespace='categories')),
     path('produtos/', include('products.urls', namespace='products')),
+    path('redessociais/', include('socialnetworks.urls', namespace='socialnetworks')),
 ]
